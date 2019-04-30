230e10fa89799312d6b608104d472c93f7b2e719
askoenig <askoenig94@gmail.com>
2019-04-30 12:38:33 -0400

    Done.

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,22 @@
 class BoatingTest
 
+  @@all = []
+
+  attr_accessor :student, :boating_test_name, :status, :instructor
+
+  def initialize(student, boating_test_name, status, instructor)
+    @student = student
+    @boating_test_name = boating_test_name
+    @status = status
+    @instructor = instructor
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,30 @@
 class Student
 
+  @@all = []
+
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_boating_test(boating_test_name, status, instructor)
+    BoatingTest.new(self, boating_test_name, status, instructor)
+  end
+
+  def self.find_student(this_first_name)
+    self.all.select {|student| student.first_name = this_first_name}
+  end
+
+  def grade_percentage
+    total_tests = BoatingTest.all.select {|test| test.student == self}
+    passed_tests = total_tests.select {|test| test.status == "passed"}
+    percent = ((passed_tests.size.to_f/total_tests.size.to_f)*100).round(2)
+    # percent.to_f
+  end
+
+
 end
